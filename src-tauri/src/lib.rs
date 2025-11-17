1856f4c03a1c47d01fc10ee12bae5125d7774633
RafaelQsantos-RQS <rafaelqsantos33@gmail.com>
2025-11-17 01:34:31 -0300

    feat: Add Tauri dialog plugin and update dependencies

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,17 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+mod commands;
+mod db_logic;
+mod models;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .plugin(tauri_plugin_dialog::init())
+        .invoke_handler(tauri::generate_handler![
+            commands::load_parquet_schema,
+            commands::get_page_data,
+            commands::get_file_metadata,
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
